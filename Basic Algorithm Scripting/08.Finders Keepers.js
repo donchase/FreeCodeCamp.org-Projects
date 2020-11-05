c4237eca6d1f15a75624360d6a3318a09806477e
Don Chase <donchase@gmail.com>
2020-11-05 11:52:58 -0500

    Commit

---
+++ Basic Algorithm Scripting/08.Finders Keepers.js
@@ -0,0 +1,22 @@
+/*
+ASSIGNMENT:
+Basic Algorithm Scripting: Finders Keepers: Passed
+Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+TESTS:
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) should return 8.
+findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) should return undefined.
+*/
+
+function findElement(arr, func) {
+    let element;
+    for (element in arr) {
+        if (func(arr[element]) == true) {
+            return arr[element];
+        }
+    }
+    return undefined;
+}
+
+console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })); // 8
+console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })); // undefined
