a38dffc8c7939ec4e0362e445248be6418ec8800
donchase <donchase@gmail.com>
2020-11-06 14:56:14 -0500

    Commit

--- Basic Algorithm Scripting/04.Return Largest Numbers in Arrays.js
+++ Basic Algorithm Scripting/04.Return Largest Numbers in Arrays.js
@@ -15,6 +15,7 @@ largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -1
 */
 function largestOfFour(arr) {
     let largestArr = [];
+    /*
     let testArr;
     for (testArr of arr) {
         let largestNum = testArr[0];
@@ -26,25 +27,43 @@ function largestOfFour(arr) {
         }
         largestArr.push(largestNum);
     }
+    */
 
+
+    for (let i = 0; i < arr.length; i++) {
+        let testArr = arr[i];
+        //console.log("testArr: " + testArr);
+        let largestNum = testArr[0];
+        //console.log("largestNum:" + largestNum);
+
+        for (let j = 0; j < testArr.length; j++) {
+            let num = testArr[j];
+            //console.log("num: " + num);
+            if (num > largestNum) {
+                largestNum = num;
+                //console.log("New largestNum: " + largestNum);
+            }
+        }
+        largestArr.push(largestNum);
+        //console.log("largestArr: " + largestArr);
+    }
     return largestArr;
 }
 
-console.log(largestOfFour([
-    [13, 27, 18, 26],
-    [4, 5, 1, 3],
-    [32, 35, 37, 39],
-    [1000, 1001, 857, 1]
-])); // should return [27, 5, 39, 1001]
-console.log(largestOfFour([
-    [4, 9, 1, 3],
-    [13, 35, 18, 26],
-    [32, 35, 97, 39],
-    [1000000, 1001, 857, 1]
-])); // should return [9, 35, 97, 1000000]
-console.log(largestOfFour([
-    [17, 23, 25, 12],
-    [25, 7, 34, 48],
-    [4, -10, 18, 21],
-    [-72, -3, -17, -10]
-])); // should return [25, 48, 21, -3]
+largestOfFour([
+        [13, 27, 18, 26],
+        [4, 5, 1, 3],
+        [32, 35, 37, 39],
+        [1000, 1001, 857, 1]
+    ])
+    /*
+    console.log(largestOfFour([
+    [13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]
+    ])); // should return [27, 5, 39, 1001]
+    console.log(largestOfFour([
+    [4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]
+    ])); // should return [9, 35, 97, 1000000]
+    console.log(largestOfFour([
+    [17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]
+    ])); // should return [25, 48, 21, -3]
+    */
---
+++ Basic Algorithm Scripting/Playing.js
@@ -0,0 +1,13 @@
+let letterArray = ["a", "b", "c", "d", "e"];
+console.log(letterArray[2]);
+letterArray[2] = "z";
+console.log(letterArray[2]);
+
+let numArr = [
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+];
+console.log(numArr[0]);
+console.log(numArr[0][1]);
