07be0fc17fdb344832b3262ac1893ee77935d245
Don Chase <donchase@gmail.com>
2020-12-19 13:33:02 -0500

    Commit

--- 05.Front End Libraries/05.Redux/06.Handle an Action in the Store.jsx
+++ 05.Front End Libraries/05.Redux/06.Handle an Action in the Store.jsx
@@ -9,3 +9,25 @@ The code editor has the previous example as well as the start of a reducer funct
 
 "use strict";
 
+const defaultState = {
+    login: false
+};
+
+const reducer = (state = defaultState, action) => {
+    // Change code below this line
+
+    if (action.type === "LOGIN") {
+        return { login: true };
+    } else {
+        return state;
+    }
+    // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+    return {
+        type: 'LOGIN'
+    }
+};
